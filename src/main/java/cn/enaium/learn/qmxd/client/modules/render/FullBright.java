fa7ce680e395c3413acc830acb6a9691ecc267ea
Enaium <32991121+Enaium@users.noreply.github.com>
2021-03-13 17:38:30 +0800

    learn2

---
+++ src/main/java/cn/enaium/learn/qmxd/client/modules/render/FullBright.java
@@ -0,0 +1,26 @@
+package cn.enaium.learn.qmxd.client.modules.render;
+
+import cn.enaium.cf4m.annotation.module.Disable;
+import cn.enaium.cf4m.annotation.module.Enable;
+import cn.enaium.cf4m.annotation.module.Module;
+import cn.enaium.cf4m.module.Category;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.glfw.GLFW;
+
+/**
+ * Project: qmxd
+ * -----------------------------------------------------------
+ * Copyright © 2020-2021 | Enaium | All rights reserved.
+ */
+@Module(value = "FullBright", key = GLFW.GLFW_KEY_L, category = Category.RENDER)
+public class FullBright {
+    @Enable
+    public void enable() {
+        Minecraft.getInstance().gameSettings.gamma = 300;
+    }
+
+    @Disable
+    public void disable() {
+        Minecraft.getInstance().gameSettings.gamma = 1;
+    }
+}
---
+++ src/main/java/cn/enaium/learn/qmxd/mixins/KeyboardListenerMixin.java
@@ -0,0 +1,25 @@
+package cn.enaium.learn.qmxd.mixins;
+
+import cn.enaium.cf4m.event.events.KeyboardEvent;
+import net.minecraft.client.KeyboardListener;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Project: qmxd
+ * -----------------------------------------------------------
+ * Copyright © 2020-2021 | Enaium | All rights reserved.
+ */
+@Mixin(KeyboardListener.class)
+public class KeyboardListenerMixin {
+    @Inject(at = @At("HEAD"), method = "onKeyEvent")
+    private void onKeyEvent(long window, int key, int p_onKeyEvent_4_, int action, int p_onKeyEvent_6_, CallbackInfo callbackInfo) {
+        if (Minecraft.getInstance().currentScreen == null && action == GLFW.GLFW_PRESS) {
+            new KeyboardEvent(key).call();
+        }
+    }
+}
